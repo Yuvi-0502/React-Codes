a4497af8d988b72a7200b82df9ee3795c28a0081
Yuvi-0502 <91358282+Yuvi-0502@users.noreply.github.com>
2022-02-21 15:19:18 +0530

    Add files via upload

---
+++ seven-side-one-context/problem.js
@@ -0,0 +1,33 @@
+// import React,{ useState } from "react";
+// const GrandChild =(props) =>{
+//   return(
+//     <div>
+//     <h3>
+//     GrandChild:
+//     </h3>
+//      <Child brand={props.brand} />
+//     </div>
+//   )
+// }
+// const Child = (props )=>{
+//   return(
+//     <div>
+//       <h2>
+//         Child : {props.brand}
+//       </h2>
+//     </div>
+//   )
+// }
+// const App =()=>{
+//   const [brandname,setBrandname]=useState("Amazon");
+//   return(
+//     <div>
+//       <h1>
+//         Hello Yuvi
+//         {/* <Child brand={brandname}/> */}
+//         <GrandChild brand={brandname}/>
+//       </h1>
+//     </div>
+//   )
+// }
+// export default App;
---
+++ seven-side-one-context/provider.js
@@ -0,0 +1,24 @@
+import React,{useState} from 'react';
+
+import PackageContext from "./context.js"
+
+const Provider= props=>{
+    const [mission,setMission]=useState({
+        mname : "Go to Russsia",
+        agent : "007",
+        accept : "Not accepted"
+    })
+    return(
+        <PackageContext.Provider 
+        value={{
+            data : mission,
+            isMissionAccepted : ()=>{
+                setMission({...mission , accept:"ACCEPTED"})
+            }
+        }}
+        >
+            {props.children}
+        </PackageContext.Provider>
+    )
+}
+export default Provider;
