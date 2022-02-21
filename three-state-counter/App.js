f4e9db4dd788f8abee73f7b070700912f987066d
Yuvi-0502 <91358282+Yuvi-0502@users.noreply.github.com>
2022-02-21 15:10:52 +0530

    Add files via upload

---
+++ three-state-counter/App.js
@@ -0,0 +1,25 @@
+import React,{useState} from "react"
+import "./App.css"
+
+const App= () =>{
+  
+  
+  const [count, setCount]=useState(0)
+
+
+
+  return(
+    <div className="App">
+      <header>
+        <h1>Counter App Using State/hooks</h1>
+      </header>
+      <h2>Current value of count is {count}</h2>
+      <button onClick={() =>{setCount(0)}}>Reset Counter</button>
+      <button onClick={() =>count>9 ? "":setCount(count + 1)}>Increase  Counter</button>
+      <button onClick={() => count>0 ? setCount(count - 1) : ""}>Decrease Counter</button>
+    </div>
+  )
+}
+
+
+export default App;
